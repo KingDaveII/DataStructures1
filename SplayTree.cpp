91f186d2538e30d0b5538a5ef1f96a52c69c166d
KingDaveII <Kopldava@gmail.com>
2017-11-28 16:12:17 +0200

    Add files via upload

---
+++ SplayTree.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructuresHW1
+//
+//  Created by DAVID KOPLEVATSKII on 28/11/2017.
+//  Copyright © 2017 DAVID KOPLEVATSKII. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ SplayTree.h
@@ -0,0 +1,13 @@
+//
+//  SplayTree.h
+//  DataStructuresHW1
+//
+//  Created by DAVID KOPLEVATSKII on 28/11/2017.
+//  Copyright © 2017 DAVID KOPLEVATSKII. All rights reserved.
+//
+
+#ifndef SplayTree_h
+#define SplayTree_h
+
+
+#endif /* SplayTree_h */
