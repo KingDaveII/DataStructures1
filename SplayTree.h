adc217d1e6af16b979c7fc4f6adf5e4c7d1c238a
KingDaveII <Kopldava@gmail.com>
2017-11-28 16:51:58 +0200

    Add files via upload

--- SplayTree.h
+++ SplayTree.h
@@ -9,5 +9,37 @@
 #ifndef SplayTree_h
 #define SplayTree_h
 
-
+template <class T>
+class SplayTree{
+    
+    
+    struct SplayTreeNode{
+    T value;
+    SplayTreeNode *left;
+    SplayTreeNode *right;
+    SplayTreeNode *dad;
+    }
+    
+    void delSubTree(SplayTreeNode* node){
+        if (node == NULL){
+            return NULL;
+        }
+        delSubTree(node->left);
+        delSubTree(node->right);
+        delete node;
+    }
+    
+    
+    
+    
+    SplayTreeNode* root;
+    
+public:
+    SplayTree() {
+        root = NULL;
+    }
+    ~SplayTree() {
+       delSubTree(root);
+    }
+}
 #endif /* SplayTree_h */
